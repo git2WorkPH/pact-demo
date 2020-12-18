57c0f44e6fcf955a6a3d6355f9490fa16f9370d9
git2WorkPH <jaycac@gmail.com>
2020-12-18 12:18:12 +1100

    modify code

--- consumer.spec.js
+++ consumer.spec.js
@@ -10,31 +10,41 @@ describe('Pact with Order API', () => {
     port: 3002,
     consumer: 'OrderClient',
     provider: 'OrderApi',
+    spec:1,
   })
+  
   before(() => provider.setup())
   after(() => provider.finalize())
+
   describe('when a call to the API is made', () => {
     before(async () => {
-      return provider.addInteraction({
-        state: 'there are orders',
-        uponReceiving: 'a request for orders',
-        withRequest: {
-          path: '/orders',
-          method: 'GET',
-        },
-        willRespondWith: {
-          body: eachLike({
-            id: 1,
-            items: eachLike({
-              name: 'burger',
-              quantity: 2,
-              value: 100,
-            }),
-          }),
-          status: 200,
-        },
-      })
+
+    const interaction =  provider.addInteraction({
+            state: 'there are orders',
+            uponReceiving: 'a request for orders',
+            withRequest: {
+              path: '/orders',
+              method: 'GET',
+            },
+            willRespondWith: {
+              headers:{
+                'Content-Type':'application/json',
+              },
+              body: eachLike({
+                id: 1,
+                items: eachLike({
+                  name: 'burger',
+                  quantity: 2,
+                  value: 100,
+                }),
+              }),
+              status: 200,
+            },
+          })
+
+      return interaction
     })
+
     it('will receive the list of current orders', async () => {
       const result = await fetchOrders()
       assert.ok(result.length)
